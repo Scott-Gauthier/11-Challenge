2357bf0b489249afcf977e1e79df3211ddcf25cd
Scott-Gauthier <gauthier.scott@gmail.com>
2022-10-31 17:35:08 -0700

    Get and write working. Need to work on delete.

--- server.js
+++ server.js
@@ -26,12 +26,39 @@ app.get('/notes', (req, res) => {
 //return dynamic page that has notes
 app.get('/api/notes', (req, res) => {
     const filename = path.join(__dirname, './db/db.json');
-    fs.readFile(filename, function(err, data){
+    fs.readFile(filename, function (err, data) {
         res.json(JSON.parse(data))
     }
     );
 });
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a record`);
+    console.log(req.body)
+    const filename = path.join(__dirname, './db/db.json');
+    // Destructuring items in req.body
+    const { title, text } = req.body;
+    // If all the required properties are present
+    if (title && text) {
+        // Variable for the object we will save
+        const newRecord = {
+            title,
+            text,
+            id: uuid(),
+        };
+        fs.readFile(filename, function (err, data) {
+            const dataWrite = JSON.parse([data]);
+            dataWrite.push(newRecord);
+            console.log(dataWrite);
+
+        fs.writeFile(filename, JSON.stringify(dataWrite), (err) =>
+            err ? console.log(err) : res.json(dataWrite)
+        );
+    }
+    );
+    };
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+    console.log(`Example app listening at http://localhost:${PORT}`);
 });
